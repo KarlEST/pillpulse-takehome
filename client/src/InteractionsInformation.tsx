4282f72837e4bfa41cc2c10b67aa30c71c701605
Karl Lääts <karl.l22ts@gmail.com>
2023-01-23 15:59:12 +0200

    add bitmoji assets

--- client/src/InteractionsInformation.tsx
+++ client/src/InteractionsInformation.tsx
@@ -1,5 +1,18 @@
 import './InteractionsInformation.css';
 
+import no1 from './assets/no1.png';
+import no2 from './assets/no2.png';
+import no3 from './assets/no3.png';
+import no4 from './assets/no4.png';
+import no5 from './assets/no5.png';
+import no6 from './assets/no6.png';
+import yes1 from './assets/yes1.png';
+import yes2 from './assets/yes2.png';
+import yes3 from './assets/yes3.png';
+import yes4 from './assets/yes4.png';
+import yes5 from './assets/yes5.png';
+import yes6 from './assets/yes6.png';
+
 interface InteractionsInformationProps {
   extraInformation: string;
   interactions: string[];
@@ -9,13 +22,14 @@ const InteractionsInformation = ({
   extraInformation,
   interactions,
 }: InteractionsInformationProps) => {
-  {
-    /* Image of the result */
-  }
+  const randomInt = Math.floor(Math.random() * 6);
+  const yesAssets = [yes1, yes2, yes3, yes4, yes5, yes6];
+  const noAssets = [no1, no2, no3, no4, no5, no6];
 
   if (interactions.length > 0) {
     return (
-      <div className="InteractionsInformation-interactions">
+      <div className="InteractionsInformation">
+        <img src={noAssets[randomInt]} alt="" className="InteractionsInformation-image" />
         {interactions.map((interaction) => (
           <p key={interaction}>💊 {interaction}</p>
         ))}
@@ -23,7 +37,14 @@ const InteractionsInformation = ({
     );
   }
 
-  return <p>{extraInformation}</p>;
+  return (
+    <div>
+      {extraInformation === 'There are no known interactions between these drugs.' ? (
+        <img src={yesAssets[randomInt]} alt="" className="InteractionsInformation-image" />
+      ) : null}
+      <p>{extraInformation}</p>
+    </div>
+  );
 };
 
 export default InteractionsInformation;
